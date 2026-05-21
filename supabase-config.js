c22426e85f811429d896a9b552b621f6683956f7
donqwsdc <nbjarneg@googlemail.com>
2026-05-21 06:56:25 +0200

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.TIMEKEEPER_SUPABASE_CONFIG = {
-  SUPABASE_URL: "https://aazogymaorvooprasjmc.supabase.co",
+  SUPABASE_URL: "https://aazogymaorvooprasjmc.supabase.com",
   SUPABASE_ANON_KEY: "sb_publishable_gGV1Tq5IjWBGEAEFJmQNxA_InRDoGjm",
 };
